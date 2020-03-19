8684bf7100d8431cb906fdab99410e363c46bcf8
yoseftuk <yoostuk@gmail.com>
2020-03-19 22:14:58 +0200

    basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { AsideComponent } from './layouts/aside/aside.component';
+import { MainComponent } from './layouts/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AsideComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
